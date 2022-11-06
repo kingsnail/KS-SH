b82677acf379591a57bdb5eeb72be3341f106ab0
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2022-11-06 16:32:08 +0300

    Create get_status.py

---
+++ python/get_status.py
@@ -0,0 +1,35 @@
+
+
+op = ""
+op .= '{ "devices": [\n'
+op .= '		{ "bulbID": 	"65539", \n'
+op .= '		  "name":    	"Nazanin's Light",\n'
+op .= '		  "brightness": "3", \n'
+op .= '		  "warmth":		"NAN%", \n'
+op .= '		  "state":		"on"\n'
+op .= '		},\n'
+op .= '		{\n'
+op .= '		  "bulbID": 	"65538",\n'
+op .= '		  "name": 		"Bedside A", \n'
+op .= '		  "brightness": "3",\n'
+op .= '		  "warmth": 	"100.0%", \n'
+op .= '		  "state": 		"off"\n'
+op .= '		}\n'
+op .= '	],\n'
+op .= '  "groups": [\n'
+op .= '		{\n'
+op .= '		"groupID":		"131077", \n'
+op .= '		"name": 		"Guest Room", \n'
+op .= '		"state": 		"off"\n'
+op .= '		},\n'
+op .= '		{\n'
+op .= '		"groupID": 		"131075",\n'
+op .= '		"name": 		"Master Bedroom", \n'
+op .= '		"state": 		"off"\n'
+op .= '		}\n'
+op .= '	]\n'
+op .= '}\n'
+
+
+print(op)
+sys.stdout.flush()
