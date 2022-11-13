81f55e62ee37c70779677875f22a89b6c99d1d7b
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2022-11-13 11:11:33 +0300

    Update KS-SH.js

--- KS-SH.js
+++ KS-SH.js
@@ -93,7 +93,7 @@ Module.register("KS-SH", {
             	
 		const d = Devs[dev].bulbID;
 		const s = Devs[dev].state;
-		statespan.onclick = this.setDevice(d, s);
+		statespan.onclick = function(){console.log("Click " + d);}
 		
 		devrow.appendChild(statespan);
 		devrow.appendChild(textspan);
