9ad0de70180e4fd5a80474c9af8fbdb5c31b4b32
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2022-11-13 13:23:29 +0300

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -42,7 +42,7 @@ module.exports = NodeHelper.create({
  
     setDeviceOn: function(cb, d) {
 	console.log("Turn " + d + " on.");
-        const pythonProcess1 = spawn('python',["/home/mark/MagicMirror/modules/KS-SH/python/tradfri-lights.py","-a", "power", "-l", d, "-v", "on]);
+        const pythonProcess1 = spawn('python',["/home/mark/MagicMirror/modules/KS-SH/python/tradfri-lights.py","-a", "power", "-l", d, "-v", "on"]);
 	pythonProcess1.stdout.on('data', function (data) { console.log("data=" + data.toString());
 							  cb(data.toString());							 
 						         });
