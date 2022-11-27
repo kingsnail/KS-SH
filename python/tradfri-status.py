cb652bab50385e4c3b27785a6e3ac6898d7d943d
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2022-11-27 12:40:10 +0300

    Update tradfri-status.py

--- python/tradfri-status.py
+++ python/tradfri-status.py
@@ -71,7 +71,7 @@ def main():
 
     time.sleep(.5)
     for sceneid in tqdm(range(len(scenes)), desc='Tradfri scenes', unit='scene'):
-        print('sceneid' + sceneid)
+        print('sceneid' + str(sceneid))
         scenegroup.append(tradfriStatus.tradfri_get_scene(hubip, apiuser, apikey,
                                                           str(scenes[sceneid])))
         
