cc42e8042dd6669194b721a59888dca4d00f9587
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2022-12-17 16:40:13 +0300

    Update KS-SH.js

--- KS-SH.js
+++ KS-SH.js
@@ -170,6 +170,8 @@ Module.register("KS-SH", {
     },
     setFullDevice: function(d, s, b, c){
         console.log("KS-SH: setFullDevice()");
+        var pl = "{ device: " + d.toString() + ", state: " + s + ", bright: " + b.toString() + ", color: " + c.toString() + "}";
+	this.sendSocketNotification('SET_DEVICE_STATE', pl);
     },
 	// this processes your data
     setDevice: function(d, s) { 
